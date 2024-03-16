05ee154e6145117d81f465808cca8088c13993cf
patmat511 <128796303+patmat511@users.noreply.github.com>
2024-03-16 14:20:27 +0100

    Time: 114 ms (40.10%), Space: 52.1 MB (87.31%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public IList<int> FindWordsContaining(string[] words, char x) {
+        var list = new List<int>();
+        for(int i = 0; i < words.Length; i++) 
+        {
+            if(words[i].Contains(x))
+                list.Add(i);
+        }
+        return list;
+    }
+}
