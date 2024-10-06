c0d813871155a2467f2977c8586f2cdee706880d
patmat511 <128796303+patmat511@users.noreply.github.com>
2024-10-06 20:34:25 +0200

    Time: 72 ms (12.54%), Space: 49.6 MB (55.75%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -4,9 +4,11 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const [v1,v2] = await Promise.all([promise1, promise2]);
+//     const [v1,v2] = await Promise.all([promise1, promise2]);
     
-    return v1 + v2;
+//     return v1 + v2;
+    
+    return (await promise1) + (await promise2);
 };
 
 /**
